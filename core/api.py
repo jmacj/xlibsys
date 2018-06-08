c4aeb4041bd15fff79162d7fc2fe774b794e1431
jmacj <jm.nz0723@gmail.com>
2018-06-08 08:44:17 +0800

    added acquisition data tables

--- core/api.py
+++ core/api.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, jsonify
 
+from markupsafe import escape
+
+from core.forms import AddAcquisitionForm
 from core.models import Acquisition
 
 from datetime import datetime, date, time
@@ -11,3 +14,10 @@ app = Blueprint('api', __name__)
 @app.route('/acquisition', methods=['GET'])
 def get_acquisition():
 	return jsonify([row for row in Acquisition.select(Acquisition).dicts()])
+
+@app.route('/acquisition/dis', methods=['GET'])
+def del_acquisition():
+	Acquisition.drop_table()
+	return jsonify([row for row in Acquisition.select(Acquisition).dicts()])
+
+
--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 
 from wtforms import StringField, PasswordField, DateTimeField, SelectField, DecimalField, IntegerField
+from wtforms.fields import DateField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import Required, ValidationError, Optional, Email, EqualTo
 
@@ -18,27 +19,34 @@ class UserLoginForm(FlaskForm):
     email_address = EmailField('Email Address', [Required()])
     password = PasswordField('Password', [Required()])
 
-class CreateEmployeeForm(FlaskForm):
-	email_address = StringField('Email Address', [Required(), Email()])
+class AddAcquisitionForm(FlaskForm):
+	bn = StringField('Book ID', [Required()])
+	title = StringField('Title', [Required()])
+	author = StringField('Author', [Required()])
+	publisher = StringField('Publisher')
+	published_on = DateField('Date Published')
+	tags = StringField('Category')
+# class CreateEmployeeForm(FlaskForm):
+# 	email_address = StringField('Email Address', [Required(), Email()])
 	
-	first_name = StringField('First Name', [Required()])
-	last_name = StringField('Last Name', [Required()])
-	middle_name = StringField('Middle Name', [Required()])
-	gender = SelectField('Gender', [Required()], choices=[('1', 'Male'), ('2', 'Female')])
-	address = StringField('Address', [Required()])
-	contact_number = StringField('Contact Number', [Required()])
-	birth_date = DateTimeField('Birth Date', [Required()], format='%Y-%m-%d')
+# 	first_name = StringField('First Name', [Required()])
+# 	last_name = StringField('Last Name', [Required()])
+# 	middle_name = StringField('Middle Name', [Required()])
+# 	gender = SelectField('Gender', [Required()], choices=[('1', 'Male'), ('2', 'Female')])
+# 	address = StringField('Address', [Required()])
+# 	contact_number = StringField('Contact Number', [Required()])
+# 	birth_date = DateTimeField('Birth Date', [Required()], format='%Y-%m-%d')
 	
-	position_id = SelectField('Position', [Required()], coerce=int)
-	schedule_id = SelectField('Schedule', [Required()], coerce=int)
-	department_id = SelectField('Department', [Required()], coerce=int)
-	employment_date = DateTimeField('Employment Date', [Required()], format='%Y-%m-%d')
-	salary = DecimalField('Salary', [Required()], places=2)
+# 	position_id = SelectField('Position', [Required()], coerce=int)
+# 	schedule_id = SelectField('Schedule', [Required()], coerce=int)
+# 	department_id = SelectField('Department', [Required()], coerce=int)
+# 	employment_date = DateTimeField('Employment Date', [Required()], format='%Y-%m-%d')
+# 	salary = DecimalField('Salary', [Required()], places=2)
 	
-	sss = StringField('SSS', [Optional()])
-	pagibig = StringField('Pag-ibig', [Optional()])
-	philhealth = StringField('PhiHealth', [Optional()])
-	tin = StringField('TIN', [Optional()])
+# 	sss = StringField('SSS', [Optional()])
+# 	pagibig = StringField('Pag-ibig', [Optional()])
+# 	philhealth = StringField('PhiHealth', [Optional()])
+# 	tin = StringField('TIN', [Optional()])
 	
-	vacation_leaves = IntegerField('Vacation Leaves', [Required()], default=0)
-	sick_leaves = IntegerField('Sick Leaves', [Required()], default=0)
+# 	vacation_leaves = IntegerField('Vacation Leaves', [Required()], default=0)
+# 	sick_leaves = IntegerField('Sick Leaves', [Required()], default=0)
